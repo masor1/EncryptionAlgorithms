d641ae0f3c4c8419917f745a3f0f58c18751df37
masor1 <masor.dev@gmail.com>
2022-11-04 14:13:32 +0300

    fix  caesar algorithm

--- app/src/main/java/com/masorone/encryptionalgorithms/EncryptionAlgorithm.kt
+++ app/src/main/java/com/masorone/encryptionalgorithms/EncryptionAlgorithm.kt
@@ -1,74 +1,160 @@
 package com.masorone.encryptionalgorithms
 
+import android.content.Context
+
 interface EncryptionAlgorithm<I, O, K> {
 
-    fun encrypt(data: I, key: K): Result
+    fun encrypt(data: I, key: K): Result<O>
 
-    fun decrypt(data: O, key: K): Result
+    fun decrypt(data: O, key: K): Result<O>
 
-    sealed class Result {
+    sealed class Result<O> : Map<O> {
 
-        object EmptyData : Result()
+        data class EmptyMessage(private val message: String) : Result<String>() {
 
-        data class UnknownSign(private val sign: String) : Result()
+            override fun map(): String {
+                return message
+            }
+        }
 
-        data class Success(private val data: String) : Result()
+        data class UnknownSign(private val message: String) : Result<String>() {
+
+            override fun map(): String {
+                return message
+            }
+        }
+
+        data class NegativeKey(private val message: String) : Result<String>() {
+
+            override fun map(): String {
+                return message
+            }
+        }
+
+        data class Success(private val data: String) : Result<String>() {
+
+            override fun map(): String {
+                return this.data
+            }
+        }
     }
 
-    interface Caesar : EncryptionAlgorithm<String, String, Int> {
+    interface Caesar : EncryptionAlgorithm<String, String, String> {
 
-        class Ru(
-            private val alphabet: String =
-                "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
+        class RuLowerCase(
+            private val provideResources: ProvideResources,
+            private val alphabet: String = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
         ) : Caesar {
 
-            override fun encrypt(data: String, key: Int): Result {
+            override fun encrypt(data: String, key: String): Result<String> {
+                if (key.isEmpty() || key == "0")
+                    return Result.Success(data)
                 if (data.isEmpty())
-                    return Result.EmptyData
-
+                    return Result.EmptyMessage(provideResources.string(R.string.error_empty_message))
+                data.forEach { ch ->
+                    if (!alphabet.contains(ch))
+                        return Result.UnknownSign(
+                            provideResources.string(
+                                R.string.error_unknown_sign,
+                                ch.toString()
+                            )
+                        )
+                }
+                var intKey = key.toInt()
+                if (intKey < 0)
+                    return Result.NegativeKey(
+                        provideResources.string(
+                            R.string.error_negative_key,
+                            key
+                        )
+                    )
+                if (intKey > alphabet.length - 1)
+                    intKey %= alphabet.length
                 var result = ""
-                data.forEach { dataStr ->
-                    if (!alphabet.contains(dataStr))
-                        return if (" " == dataStr.toString())
-                            Result.UnknownSign("_")
+                data.forEach{ chD ->
+                    alphabet.forEachIndexed { index, chA ->
+                        if (chA == chD) {
+                        result += if (index + intKey > alphabet.length - 1)
+                            alphabet[intKey + index - alphabet.length]
                         else
-                            Result.UnknownSign(dataStr.toString())
-                    alphabet.forEachIndexed { index, alphStr ->
-                        val i = index + (key * 2)
-                        if (dataStr == alphStr) {
-                            result += if (i < alphabet.length)
-                                alphabet[i]
-                            else
-                                alphabet[i - (alphabet.length + key - 1)]
+                            alphabet[index + intKey]
                         }
                     }
                 }
                 return Result.Success(result)
             }
 
-            override fun decrypt(data: String, key: Int): Result {
+            override fun decrypt(data: String, key: String): Result<String> {
+                if (key.isEmpty() || key == "0")
+                    return Result.Success(data)
                 if (data.isEmpty())
-                    return Result.EmptyData
-
+                    return Result.EmptyMessage(provideResources.string(R.string.error_empty_message))
+                data.forEach { ch ->
+                    if (!alphabet.contains(ch))
+                        return Result.UnknownSign(
+                            provideResources.string(
+                                R.string.error_unknown_sign,
+                                ch.toString()
+                            )
+                        )
+                }
+                var intKey = key.toInt()
+                if (intKey < 0)
+                    return Result.NegativeKey(
+                        provideResources.string(
+                            R.string.error_negative_key,
+                            key
+                        )
+                    )
+                if (intKey > alphabet.length - 1)
+                    intKey %= alphabet.length
                 var result = ""
-                data.forEach { dataStr ->
-                    if (!alphabet.contains(dataStr))
-                        return if (" " == dataStr.toString())
-                            Result.UnknownSign("_")
-                        else
-                            Result.UnknownSign(dataStr.toString())
-                    alphabet.forEachIndexed { index, alphStr ->
-                        val i = index - (key * 2)
-                        if (dataStr == alphStr) {
-                            result += if (i > -1)
-                                alphabet[i]
+                data.forEach{ chD ->
+                    alphabet.forEachIndexed { index, chA ->
+                        if (chA == chD)
+                            result += if (index - intKey < 0)
+                                alphabet[alphabet.length - intKey + index]
                             else
-                                alphabet[i + (alphabet.length + key - 1)]
-                        }
+                                alphabet[index - intKey]
                     }
                 }
                 return Result.Success(result)
             }
         }
     }
+}
+
+interface ProvideResources {
+
+    fun string(idRes: Int, formatArgs: String = ""): String
+
+    class Base(private val context: Context) : ProvideResources {
+
+        override fun string(idRes: Int, formatArgs: String) = with(context.resources) {
+            if (formatArgs.isEmpty())
+                getString(idRes)
+            else
+                getString(idRes, formatArgs)
+        }
+    }
+
+    class Test : ProvideResources {
+
+        override fun string(idRes: Int, formatArgs: String): String {
+            return when (idRes) {
+                R.string.error_empty_message -> "Пустое поле. " + "Введите текст."
+                R.string.error_negative_key -> "Вы ввели ключ: $formatArgs. " +
+                        "Ключ не может быть меньше 0."
+                R.string.error_unknown_sign -> "Неизвестный символ " +
+                        "\"${if (" " == formatArgs) "Пробел" else formatArgs}\"." +
+                        "Используйте строчные русские буквы."
+                else -> ""
+            }
+        }
+    }
+}
+
+interface Map<O> {
+
+    fun map(): O
 }
